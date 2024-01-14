69038658776d98a6c61290a08001638c9e8d7c0c
Classiks <danielschulz1906@gmail.com>
2024-01-14 11:09:22 +0100

    removed unnecessary books animation

--- src/ReadingTips/Content.tsx
+++ src/ReadingTips/Content.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion";
 import IBookProp from "./IBookProp";
 import Book from "./Book";
 
@@ -7,36 +6,14 @@ interface IContentProps {
 }
 export default function Content({ books }: IContentProps) {
 
-    return <motion.ul
+    
+    return <div
         className="flex flex-col flex-grow p-5"
-        variants={container}
-        initial="hidden"
-        animate="visible"
     >{
-            books.map(book => <motion.li variants={item}>
+            books.map(book => <div>
                 <Book {...book} />
                 <hr />
-            </motion.li>)
-
-    }</motion.ul>
+            <div></div>
+        </div>)
+    }</div>
 }
-
-const container = {
-    hidden: { opacity: 1, scale: 0 },
-    visible: {
-        opacity: 1,
-        scale: 1,
-        transition: {
-            delayChildren: 0.2,
-            staggerChildren: 0.4
-        }
-    }
-};
-
-const item = {
-    hidden: { y: 5, opacity: 0 },
-    visible: {
-        y: 0,
-        opacity: 1
-    }
-};
--- src/ReadingTips/ReadingTipsStep.tsx
+++ src/ReadingTips/ReadingTipsStep.tsx
@@ -1,10 +1,11 @@
+import BlendingInDiv from "../components/BlendingInDiv"
 import Content from "./Content"
 import { STEP_before_6, STEP_after_6, Systemisch_Schlippe } from "./StepBooks"
 
 export default function ReadingTips() {
     const books = [STEP_before_6, STEP_after_6, Systemisch_Schlippe]
-    return <div className="mx-auto md:w-3/4 xl:w-[60%]">
+    return <BlendingInDiv>
         <Content books={books} />
-    </div>
+    </BlendingInDiv>
 }
 
